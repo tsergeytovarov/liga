e88423ccc9390b2bf81d4bde48f471088f134b35
talequale <antonova.julia@gmail.com>
2018-06-27 17:11:02 +0300

    Добавляет таск для свг спрайта

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,23 @@
+const window = window;
+const document = document;
+let svg4everybody = svg4everybody();
+
 window.script = (() => {
+
+  svg4everybody();
+
+  let firstScreenHeight = document.querySelector(`.intro`).offsetHeight;
+  let pageContentHeight = document.querySelector(`.page-content`).offsetHeight;
+  document.addEventListener(`scroll`, function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop < firstScreenHeight) {
+      document.querySelector(`.header`).classList.remove(`dark`);
+      document.querySelector(`.header`).classList.remove(`full`);
+    } else if (scrollTop > firstScreenHeight) {
+      document.querySelector(`.header`).classList.add(`full`);
+    } else if (scrollTop > pageContentHeight) {
+      document.querySelector(`.header`).classList.add(`dark`);
+    }
+  });
 
 })(document);
